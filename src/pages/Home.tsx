8f8b55adf9f209b4f4e716acb3ee2d8fee8d0bfb
iAyomi <ayomitunde.isijola@gmail.com>
2025-06-12 00:45:41 +0100

    changes: Updated Homepage with necessary modules

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,21 @@
-import Benefit from "../components/Benefit";
-import Pricing from "../components/Pricing";
-import Testimonials from "../components/Testimonials";
-import FAQs from "../components/FAQs";
+import Header from "../modules/Header";
+import Integrations from "../modules/Integrations";
+import Benefit from "../modules/Benefit";
+import Pricing from "../modules/Pricing";
+import Testimonials from "../modules/Testimonials";
+import FAQs from "../modules/FAQs";
+import Footer from "../modules/Footer";
 
 const Home = () => {
   return (
     <div>
+      <Header />
+      <Integrations />
       <Benefit />
       <Pricing />
       <Testimonials />
       <FAQs />
+      <Footer />
     </div>
   );
 };
