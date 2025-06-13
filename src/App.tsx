ec65e1f7277b25a9f1d9f9ae913210991bf784c3
iAyomi <ayomitunde.isijola@gmail.com>
2025-06-13 15:27:46 +0100

    fix: Fixed Eslint Unused Import Val Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import NavigationBar from "./components/NavigationBar";
 import Home from "./pages/Home";
-import DemoRequest from "./pages/DemoRequest";
+// import DemoRequest from "./pages/DemoRequest";
 import "./App.css";
 import "./tailwind.css";
 
@@ -8,8 +8,8 @@ function App() {
   return (
     <div>
       <NavigationBar />
-      {/* <Home /> */}
-      <DemoRequest />
+      <Home />
+      {/* <DemoRequest /> */}
     </div>
   );
 }
